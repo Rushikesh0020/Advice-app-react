260a59b908905fc2f26ac68ac67b8ae22ff453da
Rushikesh0020 <rushikeshpatil0020@gmail.com>
2021-06-09 19:36:31 +0530

    Version 1.0

--- src/App.js
+++ src/App.js
@@ -9,16 +9,31 @@ class App extends React.Component{
        this.fetchAdvice();
     }
     //Method is a fn which belongs to class
-    fetchAdvice = () => {
-        axios.get('https://api.adviceslip.com/advice')
-        .then((response)=> {
-            const { advice } = response.data.slip;
+    // fetchAdvice = () => {
+    //     axios.get('https://api.adviceslip.com/advice')
+    //     .then((response)=> {
+    //         const { advice } = response.data.slip;
             
+    //         this.setState({advice: advice});
+    //     })
+    //     .catch((error)=> {
+    //         console.log(error);
+    //     }); 
+    // }
+    
+
+    fetchAdvice = () =>{
+        const id = Math.floor(Math.random()*100)-1
+        axios.get(`https://api.adviceslip.com/advice/${id}`)
+        .then((response)=>{
+            const data = JSON.parse(response.data+"}")
+            const { advice } = data.slip;
+
             this.setState({advice: advice});
         })
         .catch((error)=> {
             console.log(error);
-        }); 
+        });
     }
 
     render() {
